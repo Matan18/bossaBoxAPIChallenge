67eb7e707b2a492cb6ac0733ae9918b0ce25233b
Matan18 <mateusandriola18@gmail.com>
2020-08-04 12:32:09 -0300

    Create middleware of error

--- src/shared/app.ts
+++ src/shared/app.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import 'express-async-errors';
+
 import createConnection from "./infra/typeorm/connection";
 import toolRoutes from "../tools/infra/http/routes/toolRoutes.routes";
 import "./container"
+import AppError from "./errors/AppError";
 
 createConnection();
 const app = express();
@@ -11,5 +14,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/tools', toolRoutes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message
+    });
+  }
+  console.log(err);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
+
 
 export default app;
